86295a12b600604d320e73ac5eb5c1b9416c14a1
Jisie DM <jisie.david@gmail.com>
2021-03-07 19:15:04 -0800

    boilerplate for Nutshell

---
+++ src/components/auth/Login.js
@@ -0,0 +1,81 @@
+import React, { useRef } from "react"
+import { Link, useHistory } from "react-router-dom";
+import "./Login.css"
+
+
+export const Login = () => {
+    const email = useRef()
+    const password = useRef()
+    const existDialog = useRef()
+    const passwordDialog = useRef()
+
+    const history = useHistory()
+
+
+    const existingUserCheck = () => {
+        // If your json-server URL is different, please change it below!
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(_ => _.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists && exists.password === password.current.value) {
+                    // The user id is saved under the key nutshell_user in local Storage. Change below if needed!
+                    localStorage.setItem("nutshell_user", exists.id)
+                    history.push("/")
+                } else if (exists && exists.password !== password.current.value) {
+                    passwordDialog.current.showModal()
+                } else if (!exists) {
+                    existDialog.current.showModal()
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" ref={existDialog}>
+                <div>User does not exist</div>
+                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+            </dialog>
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Password does not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Application Name</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword"> Password </label>
+                        <input ref={password} type="password"
+                            id="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Nutshell } from './components/Nutshell';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Nutshell />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
