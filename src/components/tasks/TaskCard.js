13e8b12779732b227b6285fc3ea2583bc6b5c5a4
Michael Wright <info@saymoremultimedia.com>
2021-10-07 16:17:50 -0500

    feat(Nutshell): Task Details display.

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import { useHistory } from "react-router";
 import "./Task.css";
 
-export const TaskCard = ({ task, handleDeleteTask }) => {
-//   const history = useHistory();
-
+export const TaskCard = ({ task, handleDeleteTask, user }) => {
+    
+    const convertDateString = (date) => {
+        const dateString = new Date(date);
+        const formattedDate = dateString.toDateString()
+        return formattedDate;
+    }
+  
   return (
     <section className="task">
       <div className="task__card">
@@ -12,13 +18,15 @@ export const TaskCard = ({ task, handleDeleteTask }) => {
           <h3 className="task__name">{task.name}</h3>
         </section>
         <section className="task__card-body">
-          <div className="task__user">coming soon</div>
+          <div className="task__user">Owner: {user.name}</div>
           <div className="task__due-date">{task.dueDate}</div>
           <div className="task__completion-status">{task.complete}</div>
         </section>
         <section className="task__card-footer">
           <div className="task__details-button">
-            <button type="button" className="task__details">Details</button>
+              <Link to={`/tasks/${task.id}`}>
+              <button type="button" className="task__details">Details</button>
+              </Link>
           </div>
           <div className="task__edit-delete-button-block">
             <button type="button" className="button__edit">
