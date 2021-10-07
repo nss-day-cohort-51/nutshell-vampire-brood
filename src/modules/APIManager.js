74cf4bad818d7fe23430e821f364fd9f62874273
Colin Miller <colinraymiller@gmail.com>
2021-10-07 10:15:17 -0500

    updated APIManager

--- src/modules/APIManager.js
+++ src/modules/APIManager.js
@@ -1,7 +1,11 @@
 const remoteURL = "http://localhost:8088";
 
 export default class APIManager {
-    getById(target, id, expandArray) {
+    // target: json paramater to query;                     eg. "users", "artilcles", "tasks"
+    // id: target/id;                                       eg. 1
+    // expandArray: array of parameters to expand on;       eg. ["task", "article"]
+    getById(target, id, expandArray = []) {
+        //  add trailing string of expand parameters for non-empty expandArray
         let expandQuery = expandArray.length > 0 ? "?" : "";
         if (expandArray.length > 0) {
             expandArray.forEach((elem) => {
@@ -12,7 +16,7 @@ export default class APIManager {
         return fetch(url).then((res) => res.json());
     }
 
-    getAll(target, expandArray) {
+    getAll(target, expandArray = []) {
         let expandQuery = expandArray.length > 0 ? "?" : "";
         if (expandArray.length > 0) {
             expandArray.forEach((elem) => {
@@ -20,6 +24,18 @@ export default class APIManager {
             });
         }
         let url = `${remoteURL}/${target}/${expandQuery}`;
+        return fetch(url).then((res) => res.json());
+    }
+
+    getAllByUserId(target, userId, expandArray = []) {
+        let queryParamaters = "?";
+        if (expandArray.length > 0) {
+            expandArray.forEach((elem) => {
+                queryParamaters += `_expand=${elem}&`;
+            });
+        }
+        queryParamaters += `userId=${userId}`;
+        let url = `${remoteURL}/${target}/${queryParamaters}`;
         return fetch(url).then((res) => res.json());
     }
 
