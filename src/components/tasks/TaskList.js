3687ab667f320d990a0cd3b6a0c2a61725f4fb6a
Michael Wright <info@saymoremultimedia.com>
2021-10-07 13:31:23 -0500

    feat(Nutshell): Tasks List renders to DOM. Delete button works.

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// import { useHistory } from "react-router";
+import APIManager from "../../modules/APIManager";
+import { TaskCard } from "./TaskCard";
 
 export const TaskList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  //the initial state of is an empty array
+  const [tasks, setTasks] = useState([]);
+//   const history = useHistory();
+
+const API = new APIManager()
+
+const getTasks = () => {
+
+    // After the data comes back from the API, use it in setTasks function to change the state
+    return API.getAll("tasks", ["user"]).then((tasksFromAPI) => {
+      setTasks(tasksFromAPI);
+    });
+  };
+
+  const handleDeleteTask = (id) => {
+    API.delete("tasks", id).then(() =>
+      API.getAll("tasks", ["user"]).then(setTasks)
+    );
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  return (
+    <>
+      <div className="task__container">
+        {tasks.map(task => <TaskCard key={task.id} task={task} handleDeleteTask={handleDeleteTask} />)}
+      </div>
+    </>
+  );
+};
