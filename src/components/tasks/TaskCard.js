9585339d65f9356ffb59f2a560e4c72a9b47c9d3
Michael Wright <info@saymoremultimedia.com>
2021-10-07 13:31:23 -0500

    feat(Nutshell): Tasks List renders to DOM. Delete button works.

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { useHistory } from "react-router";
-import "./Task.css"
+// import { useHistory } from "react-router";
+import "./Task.css";
 
-export const TaskCard = () => {
-  const history = useHistory();
+export const TaskCard = ({ task, handleDeleteTask }) => {
+//   const history = useHistory();
 
   return (
     <section className="task">
       <div className="task__card">
         <section className="task__card-header">
-          <h3 className="task__name"></h3>
+          <h3 className="task__name">{task.name}</h3>
         </section>
         <section className="task__card-body">
-          <div className="task__user"></div>
-          <div className="task__due-date"></div>
-          <div className="task__completion-status"></div>
+          <div className="task__user">coming soon</div>
+          <div className="task__due-date">{task.dueDate}</div>
+          <div className="task__completion-status">{task.complete}</div>
         </section>
         <section className="task__card-footer">
           <div className="task__details-button">
-            <button type="button" className="task__details"></button>
+            <button type="button" className="task__details">Details</button>
           </div>
           <div className="task__edit-delete-button-block">
             <button type="button" className="button__edit">
               Edit
             </button>
-            <button type="button" className="button__delete">
+            <button type="button" className="button__delete" onClick={() => handleDeleteTask(task.id)}>
               Delete
             </button>
           </div>
