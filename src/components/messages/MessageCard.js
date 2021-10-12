ae66afdfcd221a9b7917b24aa172bccfbc56e090
Colin Miller <colinraymiller@gmail.com>
2021-10-12 11:02:25 -0500

    Public Chat: - user should be able to view a stream of public chat messages, and send to this stream - user should be able to delete their chat messages

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -1,16 +1,32 @@
 import React from "react";
+import APIManager from "../../modules/APIManager";
 import "./messages.css";
 
-export const MessageCard = ({ text, userFrom, friendId }) => {
+export const MessageCard = ({
+    text,
+    userFrom,
+    friendId,
+    messageId,
+    refreshMessages,
+}) => {
+    const API = new APIManager();
+    const deleteMessage = () => {
+        return API.delete("messages", messageId).then(() => {
+            refreshMessages();
+        });
+    };
     return (
         <div className="messageCard">
             <div className="messageCard__content">
-                <div className="messageCard__text">
-                    <p>{text}</p>
-                </div>
                 <div className="messageCard__from">
                     <strong>{userFrom.name}</strong>
                 </div>
+                <div className="messageCard__text">
+                    <p>{text}</p>
+                </div>
+            </div>
+            <div className="messageCard__interaction">
+                <button onClick={deleteMessage}>Delete</button>
             </div>
         </div>
     );
--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -3,22 +3,45 @@ import { useState, useEffect } from "react";
 import APIManager from "../../modules/APIManager";
 import { MessageCard } from "./MessageCard";
 import "./messages.css";
+import { useRef } from "react";
 
 export const MessageList = ({ activeUserId }) => {
     const [userMessages, setUserMessages] = useState([]);
     const [messageText, setMessageText] = useState("");
 
+    // const [messagesEnd, setMessagesEnd] = useState("");
+    let messagesEnd = useRef();
+
     const currentUserId = parseInt(sessionStorage.getItem("nutshell_user"));
     const API = new APIManager();
 
     const getActiveMessages = () => {
-        fetch(
-            `http://localhost:8088/messages?userToId=${currentUserId}&userToId=${activeUserId}&userId=${activeUserId}&userId=${currentUserId}&_expand=user`
-        )
-            .then((result) => result.json())
-            .then((messageObjs) => {
-                setUserMessages(messageObjs);
-            });
+        if (activeUserId == 0) {
+            return fetch(`http://localhost:8088/messages?_expand=user`)
+                .then((result) => result.json())
+                .then((messageObjs) => {
+                    setUserMessages(messageObjs);
+                });
+        } else {
+            // fetch messages that are to/from you and to/from activeUser
+            return fetch(
+                `http://localhost:8088/messages?userToId=${currentUserId}&userToId=${activeUserId}&userId=${activeUserId}&userId=${currentUserId}&_expand=user`
+            )
+                .then((result) => result.json())
+                .then((messageObjs) => {
+                    // filter for only messages that are [you -> them] or vice versa
+                    const filteredMessages = messageObjs.filter((msg) => {
+                        let isFromYou =
+                            msg.user.id == currentUserId &&
+                            msg.userToId == activeUserId;
+                        let isToYou =
+                            msg.userToId == currentUserId &&
+                            msg.user.id == activeUserId;
+                        return isToYou || isFromYou;
+                    });
+                    setUserMessages(filteredMessages);
+                });
+        }
     };
 
     const postMessage = (text) => {
@@ -36,12 +59,20 @@ export const MessageList = ({ activeUserId }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        postMessage(messageText);
+        postMessage(messageText).then(() => setMessageText(""));
     };
 
     useEffect(() => {
         getActiveMessages();
     }, [activeUserId]);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [userMessages]);
+
+    const scrollToBottom = () => {
+        messagesEnd.scrollIntoView({ behavior: "smooth" });
+    };
 
     return (
         <>
@@ -54,14 +85,25 @@ export const MessageList = ({ activeUserId }) => {
                                 key={message?.id}
                                 text={message?.text}
                                 userFrom={message?.user}
+                                messageId={message?.id}
+                                refreshMessages={getActiveMessages}
                             />
                         ))}
+                        <div
+                            style={{ float: "left", clear: "both" }}
+                            ref={(el) => {
+                                messagesEnd = el;
+                            }}
+                        ></div>
                     </div>
                 </div>
                 <div className="messageList__input">
                     <form
                         className="messageList__input-box"
-                        onSubmit={handleSubmit}
+                        onSubmit={(event) => {
+                            handleSubmit(event);
+                            // setMessageText("");
+                        }}
                     >
                         <fieldset>
                             <input
