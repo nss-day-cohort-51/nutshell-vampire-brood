5a613520d4a144ef156534c879a0f07d52f13720
Colin Miller <colinraymiller@gmail.com>
2021-10-12 22:50:52 -0500

    add cycling 't' replacements

--- src/components/tools/HelperFunctions.js
+++ src/components/tools/HelperFunctions.js
@@ -1,7 +1,33 @@
 export const RT = (string) => {
+    const d = new Date();
+    const time = d.getTime();
+    const timeRounded = Math.floor(time / 2000) % 20;
+    const symbols = [
+        "✞",
+        "τ",
+        "Τ",
+        "┳",
+        "╦",
+        "╥",
+        "☂",
+        "☺",
+        "☾",
+        "♰",
+        "♱",
+        "✞",
+        "♰",
+        "♱",
+        "✞",
+        "♰",
+        "♱",
+        "✞",
+        "⚚",
+        "ⲧ",
+    ];
+
     if (string?.trim().length > 0) {
         const regex = /t/g;
-        const cross = "✞";
+        const cross = symbols[timeRounded];
         return string.replace(regex, cross);
     } else return string;
 };
